57ac69c77ea0b0771705982f8030fbbbd3c57303
Gabriel Vian <gvian07@gmail.com>
2022-05-13 15:32:02 +0200

    Add forecast

--- src/index.js
+++ src/index.js
@@ -23,38 +23,65 @@ function processResponse() {
   const todayData = data.Conditions[0];
   const tomorrowData = data.Conditions[1];
 
+  const forecast1 = data.Forecast[0];
+  const forecast2 = data.Forecast[1];
+  const forecast3 = data.Forecast[2];
+
   c1.innerHTML = `
     <div>${getDayInfo(todayData)}</div>
     <br/>
     <div>${getDayInfo(tomorrowData)}</div>
+    <br/>
+    <div>${getForecastInfo(forecast1)}</div>
+    <br/>
+    <div>${getForecastInfo(forecast2)}</div>
+    <br/>
+    <div>${getForecastInfo(forecast3)}</div>
   `;
 
   selectStation.innerHTML = getSelectStation();
 }
 
-function getDayInfo(data) {
+function getDayInfo(dayData) {
   if (isLargeDesktop) {
     return `
         <div class="row">
             <div class="col">
-                <img src="${data.WeatherImgurl}" alt="weather"/>
+                <img src="${dayData.WeatherImgurl}" alt="weather"/>
             </div>
         
             <div class="col">
-                <h5>${data.date.split('T')[0]}: ${data.Title}</h5>
-                <p>${data.WeatherCondition}</p>
-                <p>${data.Weatherdesc}</p>
-                <p>${data.Temperatures}</p>
+                <h5>${dayData.date.split('T')[0]}: ${dayData.Title}</h5>
+                <p>${dayData.WeatherCondition}</p>
+                <p>${dayData.Weatherdesc}</p>
+                <p>${dayData.Temperatures}</p>
             </div>
         </div>
   `;
   }
 
   return `
-    <h5>${data.date.split('T')[0]}: ${data.Title}</h5>
-    <p>${data.WeatherCondition}</p>
-    <p>${data.Weatherdesc}</p>
-    <p>${data.Temperatures}</p>
+    <h5>${dayData.date.split('T')[0]}: ${dayData.Title}</h5>
+    <p>${dayData.WeatherCondition}</p>
+    <p>${dayData.Weatherdesc}</p>
+    <p>${dayData.Temperatures}</p>
+  `;
+}
+
+function getForecastInfo(dayData) {
+  return `
+    <div class="row">
+        <div class="col-2"></div>
+        <div class="col-2">
+            <img src="${dayData.WeatherImgurl}" alt="weather"/>
+        </div>
+    
+        <div class="col">
+            <h5>${dayData.date.split('T')[0]}: ${dayData.Weatherdesc}</h5>
+            <p>Min. Temperaturen: ${dayData.TempMinmin} bis ${dayData.TempMinmax} Grad</p>
+            <p>Max. Temperaturen:  ${dayData.TempMaxmin} bis ${dayData.TempMaxmax} Grad</p>
+        </div>
+    </div>
   `;
 }
 
